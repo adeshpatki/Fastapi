695a33fdadf650bf69e0b597eb6740a7f8d2123c
adeshpatki <55293285+adeshpatki@users.noreply.github.com>
2021-12-17 15:32:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import uvicorn
+from transformers import pipeline
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Sentiment(BaseModel):
+    sentiment: str
+    sentiment_probability: float
+
+
+
+nlp = pipeline("sentiment-analysis",
+                model="distilbert-base-uncased-finetuned-sst-2-english",
+                tokenizer="distilbert-base-uncased-finetuned-sst-2-english")
+def generate(data):
+    return nlp(data)
+
+
+@app.get('/')
+def get_root():
+        return{'message': 'welcome to api'}
+
+@app.get('/api/v1/st')
+def sentiment_analysis(data: str):
+    sentiment = generate(data)
+    return sentiment[0]
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
